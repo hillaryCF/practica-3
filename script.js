af5640c85d91f7d7c9e29d2092b02353c17aed7e
hillaryCF <39503176+hillaryCF@users.noreply.github.com>
2018-07-03 20:36:54 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+/*declaro SUMA */
+let SUMA = 0;
+/*declaro NUMERO*/
+let NUMERO = 2;
+/* declaro el incremento */
+let incremento = 2 
+
+while (NUMERO <=1000){
+    SUMA= NUMERO + SUMA;
+    NUMERO = NUMERO+ incremento;
+}
+
+console.log(SUMA);
+
+
